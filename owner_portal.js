ace88142c2034fe41a3d5871950120c09c791cdf
MananJiwnani <mananjiwnani@gmail.com>
2024-06-19 00:46:46 +0530

    homepage feature cards

--- owner_portal.js
+++ owner_portal.js
@@ -10,3 +10,48 @@ function toggleMenu() {
         content.classList.add("blur");
     }
 }
+
+//    different features 
+   let features = document.querySelectorAll('.feature');
+   let display;
+   let z;
+   for (const i of features) {
+        i.onclick = function(){
+            for (const j  of features) {
+                display = `.${j.id}`
+              z = document.querySelector(display);
+              if(j===i){
+                  z.classList.remove('feature_display') ;
+              }
+              else{
+                z.classList.add('feature_display') ;
+              }
+            } 
+        }
+   }
+    // preview image 
+    const x = document.querySelector('#Image');
+    let y;
+    let reader;
+    let u;
+      x.addEventListener('change',function(){
+         reader = new FileReader();
+        reader.onload= ()=>{
+           y = reader.result;
+            const j =document.querySelector('.preview')
+            j.src = y;
+           j.classList.add('img')
+            u = document.querySelector('.Image')
+            u.style.backgroundImage ="url ('"+j.src+"')"
+        }
+        reader.readAsDataURL(x.files[0]);
+      })
+
+
+
+      function changeBackground(image){
+        document.getElementById('display').innerHTML="";
+        document.getElementById('display').style.backgroundImage="url('"+image.src+"')";
+        document.getElementById('display').style.backgroundSize="cover";
+        document.getElementById('display').style.backgroundPosition="center center";
+    }
