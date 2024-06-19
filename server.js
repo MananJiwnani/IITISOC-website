d694844f4ad881507c889da9d2267d18c5f50f29
tanmaisai1208 <tanmaisaich@gmail.com>
2024-06-19 16:08:06 +0530

    server.js

--- server.js
+++ server.js
@@ -300,6 +300,14 @@
 */
 
   //Authentication
+
+  function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    return res.redirect('/login');
+  }
+
   function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
