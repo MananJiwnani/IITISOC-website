00f140e37b04c81f9c93e3ef919fcc15eacd859a
MananJiwnani <mananjiwnani@gmail.com>
2024-06-20 10:40:45 +0530

    vacancies responsiveness

--- vacancies.js
+++ vacancies.js
@@ -1,3 +1,19 @@
+// toggle hamburger
+function toggleMenu() {
+  var overlay = document.getElementById("myLinks");
+  var content = document.querySelector(".content");
+  overlay.style.paddingTop="100px";
+  if (overlay.style.width === "30vw") {
+      overlay.style.width = "0";
+      content.classList.remove("blur");
+  } else {
+      overlay.style.width = "30vw";
+      content.classList.add("blur");
+  }
+}  
+  
+  
+  
   //  location 
  const p =document.getElementsByClassName('search_text')
  const  f =document.querySelector('.location')
