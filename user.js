30a90cce22a2abec2b8b6c001aec4308451ca931
tanmaisai1208 <tanmaisaich@gmail.com>
2024-06-21 20:50:18 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,32 +1,47 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/userDb', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    match: [/^\S+@\S+\.\S+$/, 'Email is invalid'],
-  },
-  contact: {
-    type: Number,
-    required: [true, 'Contact is required'],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'Password must be at least 6 characters long'],
-  },
-  role: {
-    type: String,
-    required: [true, 'Role is required'],
-    enum: ['owner', 'tenant'],
-  }
+      name: {
+        type: String,
+        required: [true, 'Name is required'],
+      },
+      email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Email is invalid'],
+      },
+      contact: {
+        type: Number,
+        required: [true, 'Contact is required'],
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+      },
+      role: {
+        type: String,
+        required: [true, 'Role is required'],
+        enum: ['owner', 'tenant'],
+      }
 })
 
-module.exports = mongoose.model('user', userSchema)
+userSchema.statics.findAndValidate = async function (email, password) {
+    const foundUser = await this.findOne({ email });
+    if (!foundUser) {
+      return false; 
+    }
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
+
+module.exports = mongoose.model('User', userSchema);
