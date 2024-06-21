3eaa1e6f621ac8454099e3eed56286ea164e6561
tanmaisai1208 <tanmaisaich@gmail.com>
2024-06-21 20:41:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@
   const app = express();
   const server = http.createServer(app);
 
-  const path = require('path');
   const bcrypt = require('bcrypt');
   const passport = require('passport');
   const flash = require('express-flash');
@@ -16,22 +15,24 @@
   const mongoose = require('mongoose');
 
   const initializePassport = require('./passport');
-  
-  // mongoose.connect(process.env.MONGO_URI)
-  // .then(() => console.log('Connected to MongoDB'))
-  // .catch((err) => console.error('Error connecting to MongoDB:', err));
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-  const user = require('./user');
-  const property = require('./property');
-  
-  initializePassport(
-    passport,
-    async email => await user.findOne({ email: email }),
-    async id => await user.findById(id) 
-  )
+  mongoose.connect('mongodb://localhost:27017/userDb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+  });
+
+  const User = require('./user');
+  const Property = require('./property');
   
   app.set('view-engine','ejs');
-  app.use(express.urlencoded({ extended: false }));
+  app.set('views','views');
+  app.use(express.static('public'));
+  
+  app.use(express.urlencoded({ extended: true }));
   app.use(flash());
   app.use(express.json());
   app.use(session({
@@ -44,71 +45,95 @@
   app.use(passport.session());
   app.use(methodOverride('_method'));
   
+  const checkAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    next()
+  }
+
+  const checkNotAuth = (req,res,next) => {
+    if(req.session.user_id){
+        return res.redirect('/')
+    }
+    next()
+  }
+  
   // allow user to go to homepage even if he is not loggined
   // but make sure that he wont be able to access the 
-  // search button(goes to vacancies page),owner portal,tenant portal.
-  app.get('/',(req,res) => {
+  // vacancies page,owner portal,tenant portal.
+  app.get('/', (req, res) => {
     res.render('home.ejs');
   });
 
-  app.get('/vacancies', checkAuthenticated, (req, res) => {
+  app.get('/vacancies',checkAuth, (req, res) => {
     res.render('vacancies.ejs');
   });
   
-  app.get('/ownerportal', checkAuthenticated, checkRole('owner'), (req, res) => {
-    res.render('owner.ejs');
+  app.get('/ownerportal',checkAuth, checkRole('owner'), (req, res) => {
+    res.render('ownerportal.ejs');
   });
 
-  app.get('/tenantportal', checkAuthenticated, checkRole('tenant'), (req, res) => {
-    res.render('tenant.ejs');
+  app.get('/tenantportal',checkAuth, checkRole('tenant'), (req, res) => {
+    res.render('tenantportal.ejs');
   });
 
   //myprofie is in owner portal
-  app.get('/myprofile', checkAuthenticated, checkRole('owner'), (req, res) => {
+  app.get('/myprofile',checkAuth, checkRole('owner'), (req, res) => {
     res.render('myprofile.ejs');
   });
 
-  app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs');
+  app.get('/login', (req, res) => {
+    const error = req.flash('error');
+    res.render('login.ejs', { error });
   });
   
-  app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs');
-  });
-
-  //submit button in register page should take to login page
-  app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      const newUser = new user({
-        name: req.body.name,
-        email: req.body.email,
-        contact: req.body.contact,
-        password: hashedPassword,
-        role: req.body.role
-      })
-      const saveUser = await newUser.save();
-      req.session.userId = saveUser._id;
-      return res.redirect('/login');
-    } catch {
-        return res.redirect('/register');
+  app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const foundUser = await User.findAndValidate(email, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/');
     }
+    else {
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        req.flash('error', 'Incorrect password');
+        res.redirect('/login');
+      } else {
+        req.flash('error', 'No user with that email. Please register first.');
+        res.redirect('/register');
+      }
+    }
+  }) 
+  
+  app.get('/register', (req, res) => {
+    const error = req.flash('error');
+    res.render('register.ejs', { error });
   });
   
-  app.delete('/logout', (req, res) => {
-    req.logOut();
-    return res.redirect('/login');
-  });
+  app.post('/register', checkNotAuth, async (req, res) => {
+    const { name, email, contact,password, role } = req.body;
+    const user = new User({ name, email, contact, password, role })
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/login')
+})
+
+  app.post('/logout',checkAuth, (req, res) => {
+    req.session.user_id = null;
+    res.redirect('/login');
+  })
   
   // Adding Properties
-  app.get('/addproperties', checkAuthenticated, checkRole(owner), (req, res)=>{
+  app.get('/addproperties',checkAuth, checkRole('owner'), (req, res)=>{
     res.render('addproperties.ejs');
   });
 
-  app.post('/addproperties', checkAuthenticated, checkRole(owner), async (req, res) => {
+  app.post('/addproperties',checkAuth, checkRole('owner'), async (req, res) => {
     try {
-      const newProperty = new property({
-        owner: req.session.userId, 
+      const newProperty = new Property({
+        owner: req.session.user_id, 
         ownerName: req.body.ownerName,
         propertyType: new RegExp(req.body.propertyType, 'i'),
         subCategory: new RegExp(req.body.subCategory, 'i'),
@@ -132,23 +157,23 @@
   });
 
   // My properties page for owner to see his properties
-  app.get('/myproperties', checkAuthenticated, checkRole(owner), (req, res) => {
+  app.get('/myproperties',checkAuth, checkRole('owner'), (req, res) => {
       res.render('myproperties.ejs');
   });
 
-  app.get('/api/myproperties', checkAuthenticated, checkRole(owner), async (req, res) => {
+  app.get('/api/myproperties',checkAuth, checkRole('owner'), async (req, res) => {
     try {
-      const properties = await property.find({ owner: req.session.userId });
+      const properties = await Property.find({ owner: req.session.user_id });
       res.status(200).json(properties);
     } catch (error) {
       res.status(404).send(error.message);
     }
   });
   
-  app.post('/myproperties/:id', checkAuthenticated, checkRole('owner'), async (req, res) => {
+  app.post('/myproperties/:id',checkAuth, checkRole('owner'), async (req, res) => {
     try {
       const propertyId = req.params.id;
-      const updatedProperty = await property.findByIdAndUpdate(
+      const updatedProperty = await Property.findByIdAndUpdate(
         propertyId,
         { rentedOut: true },
         { new: true }
@@ -161,11 +186,8 @@
       res.status(500).send('Internal server error');
     }
   });
-  
-  // when user submits the search form in home page it redirects to vacancies page
-  // next if someone is trying to access get request to vacancies path
-  // server renders vacancies.ejs file which in turn calls /api/vacancies. 
-  app.post('/', checkAuthenticated, (req, res) =>{
+   
+  app.post('/',checkAuth, (req, res) =>{
     try{
       const query = {};
       if (req.body.city) query.city = new RegExp(req.body.city, 'i');
@@ -187,15 +209,15 @@
     }
   });
 
-  app.get('/vacancies', checkAuthenticated, (req, res) => {
+  app.get('/vacancies',checkAuth, (req, res) => {
     const query = req.session.query || {};
-    res.render('vacancies.ejs', {query});
+    res.render('vacancies.ejs');
   });
 
-  app.get('/api/vacancies', checkAuthenticated, async (req, res) => {
+  app.get('/api/vacancies',checkAuth, async (req, res) => {
     try {
       const query = req.session.query || {};
-      const properties = await property.find(query);
+      const properties = await Property.find(query);
 
       res.status(200).json(properties);
     } catch (error) {
@@ -203,7 +225,7 @@
     }
   });
 
-  app.post('/vacancies', checkAuthenticated, (req, res) => {
+  app.post('/vacancies',checkAuth, (req, res) => {
     try{
       const query = {};
       if (req.body.city) query.city = new RegExp(req.body.city, 'i');
@@ -225,72 +247,28 @@
     }
   })
 
-  app.post('/api/vacancies/:id', checkAuthenticated, async (req, res) => {
+  app.post('/api/vacancies/:id',checkAuth, async (req, res) => {
     try {
       const propertyId = req.params.id;
-      const userId = req.session.userId; 
-      const property = await property.findById(propertyId);
-      const tenant = await user.findById(userId);
+      const userId = req.session.user_id; 
+      const property = await Property.findById(propertyId);
   
       if (!property) {
         return res.status(404).send('Property not found');
       }
-  
-      const notification = new Notification({
-        userId: property.owner,
-        message: `<b>A tenant has applied for your property</b>: ${property.description}`,
-        propertyId: property._id,
-        applicantId: tenant._id,
-        applicantName: tenant.name,
-      });
-  
-      await notification.save();
-  
       res.status(200).send('Application sent successfully');
     } catch (error) {
       res.status(500).send('Internal server error');
     }
   });
 
-  app.get('/dashboard', checkAuthenticated, checkRole('owner'), async (req, res) => {
-    try {
-      const notifications = await Notification.find({ userId: req.session.userId });
-      res.render('dashboard', { notifications });
-    } catch (error) {
-      res.status(500).send('Internal server error');
-    }
-  });
-
-
-  //Authentication
-  function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    return res.redirect('/login');
-  }
-  
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/');
-    }
-    next();
-  }
-
   // Authorization
-  function checkAuthorization(req, res, next) {
-    if (!req.isAuthenticated()) {
-        return res.status(401).send('Unauthorized: User not logged in');
-    }
-    next();
-  }
-  
   function checkRole(role) {
     return function(req, res, next) {
-        if (req.isAuthenticated() && req.user.role === role) {
+        if (req.user.role === role) {
             return next();
         }
-        res.status(402).send(`Unauthorized: Only ${role}s can access this page`);
+        res.status(403).send(`Unauthorized: Only ${role}s can access this page`);
     };
   }
 
