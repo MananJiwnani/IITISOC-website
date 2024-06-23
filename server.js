7a0746af7c3bc445abba5fe8a39f5deae7f8f3fe
Arjunkumar32 <arjunkumarb070@gmail.com>
2024-06-23 11:13:20 +0530

    New Brach

--- server.js
+++ server.js
@@ -30,7 +30,7 @@
   
   app.set('view-engine','ejs');
   app.set('views','views');
-  app.use(express.static('public'));
+  app.use(express.static(path.join(__dirname,"../public")));
   
   app.use(express.urlencoded({ extended: true }));
   app.use(flash());
