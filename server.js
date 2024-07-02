6c43842ec32f4ea6510d36fe13556c547d217b7e
tanmaisai1208 <tanmaisaich@gmail.com>
2024-07-02 19:59:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -217,6 +217,7 @@ app.post('/addproperties',checkAuth, checkRole('owner'), async (req, res) => {
 app.get('/myProperties',checkAuth, checkRole('owner'), async(req, res) => {
   try {
     const rentals = await Property.find({ ownerName: req.session.NAME }).populate(['subCategory', 'propertyType', 'address', 'city', 'state', 'price']);
+    rentals = JSON.stringify(rentals);
     res.render('myproperties.ejs', { properties: rentals });
   } catch (err) {
     res.status(500).send(err);
