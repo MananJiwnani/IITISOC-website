c517e8781be9b36a00925ce50398a331c2e1d56d
tanmaisai1208 <tanmaisaich@gmail.com>
2024-07-04 12:45:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.post('/addproperties',checkAuth, checkRole('owner'), async (req, res) => {
 // My properties page for owner to see his properties
 app.get('/myProperties',checkAuth, checkRole('owner'), async(req, res) => {
   try {
-    let rentals = await Property.find({ ownerName: req.session.NAME }).populate(['subCategory', 'propertyType', 'address', 'city', 'state', 'price']);
+    let rentals = await Property.find({ owner: req.session.user_id }).populate(['subCategory', 'propertyType', 'address', 'city', 'state', 'price']);
     // rentals = JSON.stringify(rentals);
     res.render('myproperties.ejs', { properties: rentals });
   } catch (err) {
