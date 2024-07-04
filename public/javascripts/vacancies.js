58ee8808b7e38b6ff4bd80dada5be9b37ace2c67
tanmaisai1208 <tanmaisaich@gmail.com>
2024-07-04 12:37:44 +0530

    Update vacancies.js

--- public/javascripts/vacancies.js
+++ public/javascripts/vacancies.js
@@ -64,58 +64,6 @@ const redirect = (url) => {
     window.location.href = url;
 }
 
-
-
-
-async function fetchProperties() {
-    try {
-        const response = await fetch('/api/vacancies');
-        if (!response.ok) {
-            throw new Error('Failed to fetch properties');
-        }
-        const properties = await response.json();
-        displayProperties(properties);
-    } catch (error) {
-        console.error('Error fetching properties:', error);
-        document.getElementById('properties-list').innerText = 'Error fetching properties';
-    }
-}
-
-
-// display property func
-function displayProperties(properties) {
-    const propertiesList = document.querySelector('.listings');
-    propertiesList.innerHTML = '';
-
-    if (properties.length === 0) {
-        propertiesList.innerText = 'No properties found';
-        return;
-    }
-
-    properties.forEach(property => {
-        const propertyItem = document.createElement('div');
-        propertyItem.className = 'grid-item';
-
-        propertyItem.innerHTML = `
-                <div class="card">
-                    <div class="image">
-                        ${property.images.map(image => `<img src="${image}" alt="Property Image" class="card-img">`).join('')}
-                    </div>
-                    <div class="card-content">
-                        <h2 class="card-title">${property.subCategory} ${property.propertyType}</h2>
-                        <p class="card-location">${property.address}, ${property.city}, ${property.state}</p>
-                        <p class="card-price">₹${property.price}/month</p>
-                        <a href="/property"><button class="card-button">View Details</button></a>
-                    </div>
-                </div>`;
-
-        if (!property.rentedOut) {
-            propertiesList.appendChild(propertyItem);
-        }
-    });
-}
-
-
 //to apply for property
 async function applyProperty(propertyId) {
     try {
@@ -131,10 +79,3 @@ async function applyProperty(propertyId) {
         alert('Error applying for property');
     }
 }
-
-
-
-document.addEventListener('DOMContentLoaded', fetchProperties);
-
-
-
