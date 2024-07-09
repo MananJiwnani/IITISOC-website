e412395b502c87525d9b361417a5bc179ca32a4e
tanmaisai1208 <tanmaisaich@gmail.com>
2024-07-09 21:02:02 +0530

    Update paymentController.js

--- paymentController.js
+++ paymentController.js
@@ -1,4 +1,4 @@
-// const Razorpay = require('razorpay'); 
+const Razorpay = require('razorpay'); 
 // const { RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY } = process.env;
 
 // const razorpayInstance = new Razorpay({
@@ -6,51 +6,51 @@
 //     key_secret: RAZORPAY_SECRET_KEY
 // });
 
-// const renderProductPage = async(req,res)=>{
-//     try {
-//         res.render('product');
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+const renderProductPage = async(req,res)=>{
+    try {
+        res.render('vacancies.ejs');
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
-// const createOrder = async(req,res)=>{
-//     try {
-//         const amount = req.body.amount*100
-//         const options = {
-//             amount: amount,
-//             currency: 'INR',
-//             receipt: 'razorUser@gmail.com'
-//         }
+const createOrder = async(req,res)=>{
+    try {
+        const amount = req.body.price*100
+        const options = {
+            price: price,
+            currency: 'INR',
+            receipt: 'razorUser@gmail.com'
+        }
 
-//         razorpayInstance.orders.create(options, 
-//             (err, order)=>{
-//                 if(!err){
-//                     res.status(200).send({
-//                         success:true,
-//                         msg:'Order Created',
+        razorpayInstance.orders.create(options, 
+            (err, order)=>{
+                if(!err){
+                    res.status(200).send({
+                        success:true,
+                        msg:'Order Created',
 //                         order_id:order.id,
-//                         amount:amount,
+                        price:price,
 //                         key_id:RAZORPAY_ID_KEY,
 //                         product_name:req.body.name,
 //                         description:req.body.description,
-//                         contact:"8567345632",
-//                         name: "Sandeep Sharma",
-//                         email: "sandeep@gmail.com"
-//                     });
-//                 }
-//                 else{
-//                     res.status(400).send({success:false,msg:'Something went wrong!'});
-//                 }
-//             }
-//         );
+                        contact:"9515350605",
+                        name: "Tanmai Sai",
+                        email: "tanmaisaich@gmail.com"
+                    });
+                }
+                else{
+                    res.status(400).send({success:false,msg:'Something went wrong!'});
+                }
+            }
+        );
 
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
-// module.exports = {
-//     renderProductPage,
-//     createOrder
-// }
+module.exports = {
+    renderProductPage,
+    createOrder
+}
