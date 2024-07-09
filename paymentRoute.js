415a0fa2f8922480e4628b43071bb6fbdb059c09
tanmaisai1208 <tanmaisaich@gmail.com>
2024-07-09 17:18:09 +0530

    Update paymentRoute.js

--- paymentRoute.js
+++ paymentRoute.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const payment_route = express();
-
+const paymentController = require('./paymentController');
+const path = require('path');
 const bodyParser = require('body-parser');
+
 payment_route.use(bodyParser.json());
 payment_route.use(bodyParser.urlencoded({ extended:false }));
 
-const path = require('path');
 
 payment_route.set('view engine','ejs');
 payment_route.set('views',path.join(__dirname, '../views'));
 
-const paymentController = require('../controllers/paymentController');
 
 payment_route.get('/', paymentController.renderProductPage);
 payment_route.post('/createOrder', paymentController.createOrder);
