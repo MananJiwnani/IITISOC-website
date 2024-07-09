bd111c4657f668c63cc8797ce54930f737cc32ac
tanmaisai1208 <tanmaisaich@gmail.com>
2024-07-09 19:50:03 +0530

    Update paymentController.js

--- paymentController.js
+++ paymentController.js
@@ -1,9 +1,9 @@
 const Razorpay = require('razorpay'); 
-const { RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY } = process.env;
+// const { RAZORPAY_ID_KEY, RAZORPAY_SECRET_KEY } = process.env;
 
 const razorpayInstance = new Razorpay({
-    key_id: RAZORPAY_ID_KEY,
-    key_secret: RAZORPAY_SECRET_KEY
+    // key_id: RAZORPAY_ID_KEY,
+    // key_secret: RAZORPAY_SECRET_KEY
 });
 
 const renderProductPage = async(req,res)=>{
