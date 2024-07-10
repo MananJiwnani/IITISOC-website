4dec9810f4d4392c9daae4157dd4c2b7a703620f
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-10 15:59:18 +0530

    ok

--- server.js
+++ server.js
@@ -100,7 +100,8 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: process.env.NODE_ENV === 'production' },
+  // cookie: { secure: process.env.NODE_ENV === 'production' },
+  cookie: { secure: false }
 }))
 app.use(passport.initialize());
 app.use(passport.session());
