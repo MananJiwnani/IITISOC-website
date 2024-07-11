193f91e47d33cdae54e93e99ede8cf7207d0ddc1
MananJiwnani <mananjiwnani@gmail.com>
2024-07-12 01:06:37 +0530

    rentestimate

--- public/javascripts/owner_portal.js
+++ public/javascripts/owner_portal.js
@@ -57,3 +57,104 @@ async function rentOutProperty(propertyId) {
            i.classList.toggle("anime2");
    }
    
+
+//    rent estimate
+document.getElementById('rent_estimate').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const propertyType = document.getElementById('propertyTypeRE').value;
+    const location = document.getElementById('location').value;
+    const age = document.getElementById('age').value;
+    console.log(propertyType)
+    console.log(location)
+    console.log(age)
+    const currentPrice = parseFloat(document.getElementById('currentPrice').value);
+
+    var yield=3.0; // Default
+    console.log(typeof(yield));
+
+    if (propertyType === 'Flat') {
+        if (location === 'Metropolitan Cities') {
+            switch (age) {
+                case '0-1 yrs':
+                    yield = 3.00;
+                    break;
+                case '1-2 yrs':
+                    yield = 2.90;
+                    break;
+                case '2-3 yrs':
+                    yield = 2.75;
+                    break;
+                case '3-4 yrs':
+                    yield = 2.60;
+                    break;
+                case '4 yrs+':
+                    yield = 2.50;
+                    break;
+            }
+        } else if (location === 'Tier 2 Cities') {
+            switch (age) {
+                case '0-1 yrs':
+                    yield = 3.50;
+                    break;
+                case '1-2 yrs':
+                    yield = 3.40;
+                    break;
+                case '2-3 yrs':
+                    yield = 3.35;
+                    break;
+                case '3-4 yrs':
+                    yield = 3.10;
+                    break;
+                case '4 yrs+':
+                    yield = 3.00;
+                    break;
+            }
+        }
+    } else if (propertyType === 'Villa') {
+        if (location === 'Metropolitan Cities') {
+            switch (age) {
+                case '0-1 yrs':
+                    yield = 2.50;
+                    break;
+                case '1-2 yrs':
+                    yield = 2.30;
+                    break;
+                case '2-3 yrs':
+                    yield = 2.35;
+                    break;
+                case '3-4 yrs':
+                    yield = 2.30;
+                    break;
+                case '4 yrs+':
+                    yield = 2.25;
+                    break;
+            }
+        } else if (location === 'Tier 2 Cities') {
+            switch (age) {
+                case '0-1 yrs':
+                    yield = 3.00;
+                    break;
+                case '1-2 yrs':
+                    yield = 2.90;
+                    break;
+                case '2-3 yrs':
+                    yield = 2.75;
+                    break;
+                case '3-4 yrs':
+                    yield = 2.70;
+                    break;
+                case '4 yrs+':
+                    yield = 2.65;
+                    break;
+            }
+        }
+    }
+    console.log(yield)
+
+    // Calculate estimated rent
+    const rent = (currentPrice* yield) / 1200;
+    
+    // Display estimated rent to user
+    alert(`Estimated Rent: ${rent.toFixed(0)}`); // Displaying rent rounded to 0 decimal places
+});
