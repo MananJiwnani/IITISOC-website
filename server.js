44f3bada63e4cb88fd3a53c9099232a1a125a73e
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-11 17:56:20 +0530

    ok

--- server.js
+++ server.js
@@ -353,7 +353,7 @@ app.post('/addproperties', upload.single('image'), (req, res, next) => {
       petPolicy: req.body.petPolicy,
       carpetArea: req.body.carpetArea,
       image: {
-        data: fs.readFileSync(path.join(__dirname, 'uploads', req.file.filename)),
+        data: fs.readFileSync(path.join(__dirname, 'uploads')),
         contentType: req.file.mimetype
       },
       rentedOut: false,
