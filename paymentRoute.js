89a500b98bf91389282d0fb7ab281ce3ee202487
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-14 12:33:46 +0530

    ok

--- paymentRoute.js
+++ paymentRoute.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const payment_route = express();
-// const paymentController = require('./paymentController');
-// const path = require('path');
-// const bodyParser = require('body-parser');
+const express = require('express');
+const payment_route = express();
+const paymentController = require('./paymentController');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-// payment_route.use(bodyParser.json());
-// payment_route.use(bodyParser.urlencoded({ extended:false }));
-
-
-// payment_route.set('view engine','ejs');
-// payment_route.set('views',path.join(__dirname, 'views'));
+payment_route.use(bodyParser.json());
+payment_route.use(bodyParser.urlencoded({ extended:false }));
 
 
-// payment_route.get('/vacancies', paymentController.renderProductPage);
-// payment_route.post('/createOrder', paymentController.createOrder);
+payment_route.set('view engine','ejs');
+payment_route.set('views',path.join(__dirname, 'views'));
 
-// module.exports = payment_route;
+
+payment_route.get('/vacancies', paymentController.renderProductPage);
+payment_route.post('/createOrder', paymentController.createOrder);
+
+module.exports = payment_route;
