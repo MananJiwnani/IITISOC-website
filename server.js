349d739ac5385ef793f0523281d98976c9dbc2fd
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-15 12:31:54 +0530

    ok

--- server.js
+++ server.js
@@ -154,9 +154,9 @@ app.post('/createOrder',checkAuth, async(req, res)=> {
           amount: amount, 
           key_id: RAZORPAY_ID_KEY,
           product_name: req.body.name,
-          contact: user.contact,
-          name: user.name,
-          email: user.email,
+          contact: 9515350605,
+          name: tanmaisai,
+          email: tanmaisaich,
         });
       } else {
         console.error('Error creating Razorpay order:', err);
