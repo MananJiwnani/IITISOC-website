d08ef3fc286eab001247e87d8fb7526916fbcaef
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-16 13:15:44 +0530

    ok

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.post('/createOrder',checkAuth, async(req, res)=> {
         
         res.status(200).send({
           success: true,
-          msg: 'PAYMENT INITIATED AND TENANT ASSIGNED',
+          msg: 'ORDER CREATED',
           order_id: order.id,
           amount: amount, 
           key_id: RAZORPAY_ID_KEY,
