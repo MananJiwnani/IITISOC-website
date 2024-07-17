26082ea9dab33146cda44283890a3a815fa8d437
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-17 14:33:17 +0530

    ok

--- server.js
+++ server.js
@@ -187,6 +187,7 @@ app.post('/unRent', checkAuth, async(req, res) => {
     const id = req.body.property_id;
     await Property.findByIdAndUpdate(id, { rentedOut: false });
     req.session.unrented = "unrented";
+    res.redirect('/myProperties');
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ success: false, msg: 'Internal Server Error' });
@@ -411,8 +412,7 @@ app.get('/vacancies/:id', checkAuth, async (req, res) => {
       return res.status(404).send('Property not found');
     }
 
-    res.render('property.ejs', { property: properties, ROLE: role, rented: rented, message: req.session.unrented});
-    delete req.session.unrented;
+    res.render('property.ejs', { property: properties, ROLE: role, rented: rented});
   } catch (error) {
     res.status(500).send('Internal server error');
   }
@@ -424,7 +424,8 @@ app.get('/myProperties',checkAuth, checkRole('owner'), async(req, res) => {
   try {
     let rentals = await Property.find({ owner: req.session.user_id }).populate(['subCategory', 'propertyType', 'address', 'city', 'state', 'price']);
     // rentals = JSON.stringify(rentals);
-    res.render('myproperties.ejs', { properties: rentals });
+    res.render('myproperties.ejs', { properties: rentals, message: req.session.unrented });
+    delete req.session.unrented;
   } catch (err) {
     res.status(500).send(err);
   }
