cfb179c1635f480abf9c29511c9d5b08182a7f02
Vasudha5812 <vasudhabhuva1296@gmail.com>
2024-07-16 22:08:21 -0500

    open

--- public/javascripts/add_properties.js
+++ public/javascripts/add_properties.js
@@ -32,31 +32,31 @@ for (const i of features) {
 
 
 // preview image 
-const xP = document.querySelector('#Image');
-let y;
-let reader;
-let u;
-xP.addEventListener('change', function () {
-    reader = new FileReader();
-    reader.onload = () => {
-        y = reader.result;
-        const j = document.querySelector('.preview')
-        j.src = y;
-        j.classList.add('img')
-        u = document.querySelector('.Image')
-        u.style.backgroundImage = "url ('" + j.src + "')"
-    }
-    reader.readAsDataURL(xP.files[0]);
-})
+// const xP = document.querySelector('#Image');
+// let y;
+// let reader;
+// let u;
+// xP.addEventListener('change', function () {
+//     reader = new FileReader();
+//     reader.onload = () => {
+//         y = reader.result;
+//         const j = document.querySelector('.preview')
+//         j.src = y;
+//         j.classList.add('img')
+//         u = document.querySelector('.Image')
+//         u.style.backgroundImage = "url ('" + j.src + "')"
+//     }
+//     reader.readAsDataURL(xP.files[0]);
+// })
 
 
 
-function changeBackground(image) {
-    document.getElementById('display').innerHTML = "";
-    document.getElementById('display').style.backgroundImage = "url('" + image.src + "')";
-    document.getElementById('display').style.backgroundSize = "cover";
-    document.getElementById('display').style.backgroundPosition = "center center";
-}
+// function changeBackground(image) {
+//     document.getElementById('display').innerHTML = "";
+//     document.getElementById('display').style.backgroundImage = "url('" + image.src + "')";
+//     document.getElementById('display').style.backgroundSize = "cover";
+//     document.getElementById('display').style.backgroundPosition = "center center";
+// }
 
 
 async function rentOutProperty(propertyId) {
