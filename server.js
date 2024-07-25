a0f60471cfdf37b27cb5af038fd2355ce3126b77
Arjunkumar32 <arjunkumarb070@gmail.com>
2024-07-25 21:59:03 +0530

    k

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const Grid = require("gridfs-stream");
 const { GridFsStorage } = require('multer-gridfs-storage');
 const connection = require("./db");
 
-mongoose.connect('mongodb+srv://IITISoC:0KPwekTMfrOhdiiZ@rentestatedb.d7qhrpe.mongodb.net/?retryWrites=true&w=majority&appName=rentestatedb').then(() => {
+mongoose.connect('mongodb+srv://jonty:ic1HDE142HxSTHZf@cluster0.tluj8rn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Failed to connect to MongoDB', err);
