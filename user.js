32384b664eaa1abacd3007b2db4987ef8605c23e
chennagiri tanmai sai <tanmaisaich@gmail.com>
2024-07-26 14:44:24 +0530

    ok

--- user.js
+++ user.js
@@ -37,12 +37,18 @@ const userSchema = new mongoose.Schema({
       },
       password: {
         type: String,
-        required: [true, 'Password is required'],
+        // required: [true, 'Password is required'],
+        required: function() {
+          return !this.googleId;
+        },
         minlength: [6, 'Password must be at least 6 characters long'],
       },
       role: {
         type: String,
-        required: [true, 'Role is required'],
+        // required: [true, 'Role is required'],
+        required: function() {
+          return !this.googleId;
+        },
         enum: ['owner', 'tenant'],
       }
       
